8921690baf03c283eb63b35f5d98b994d3ac1de0
satvikigutta <satvikigutta96@gmail.com>
2026-07-06 14:35:18 -0700

    Remove pandas import and CSV loading
    Remove unnecessary import statements and file loading.

--- week 2_3 deliverable.py
+++ week 2_3 deliverable.py
@@ -1,6 +1,1 @@
-import pandas as pd
 
-sold = pd.read_csv("/Users/YourName/Downloads/combined_sold_residential.csv")
-listings = pd.read_csv("/Users/YourName/Downloads/combined_listings_residential.csv")
-
-print("done loading!")
