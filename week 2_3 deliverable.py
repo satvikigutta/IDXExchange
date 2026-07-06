95c4609e89340847b9b9966fb97d017c4bee315d
satvikigutta <satvikigutta96@gmail.com>
2026-07-06 14:04:10 -0700

    Add files via upload

---
+++ week 2_3 deliverable.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+sold = pd.read_csv("/Users/YourName/Downloads/combined_sold_residential.csv")
+listings = pd.read_csv("/Users/YourName/Downloads/combined_listings_residential.csv")
+
+print("done loading!")
